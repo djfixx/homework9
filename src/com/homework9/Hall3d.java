96ce60a6565a899a864798651c67933b48fd6071
djfixx <djfixx83@gmail.com>
2022-09-04 22:12:40 +0300

    1

--- src/com/homework9/Hall3d.java
+++ src/com/homework9/Hall3d.java
@@ -2,7 +2,7 @@ package com.homework9;
 
 public class Hall3d extends CinemaHall {
     protected boolean supportImax;
-    private boolean support4dx;
+    protected boolean support4dx;
     Hall3d(String hallName, boolean surroundSound, boolean supportImax, boolean support4dx){
         this.hallName = hallName;
         this.surroundSound = surroundSound;
